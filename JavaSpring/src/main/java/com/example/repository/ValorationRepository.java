795217e9eff0b72b602a32e7b225b5a8fd0efd7f
BNRS <BNRS@gft.com>
2017-04-04 09:03:47 +0200

    clear

--- JavaSpring/src/main/java/com/example/repository/ValorationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.entities.Valoration;
-
-public interface ValorationRepository extends CrudRepository<Valoration, Long> {
-
-}
