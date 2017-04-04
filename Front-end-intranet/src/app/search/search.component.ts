a9a7811dbd4ea6bbe4dbe6a6f5283ac648dac33c
OEEI <Othmane.El-Ati@gft.com>
2017-04-04 13:02:16 +0200

    Search master detail

--- Front-end-intranet/src/app/search/search.component.ts
+++ Front-end-intranet/src/app/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Playlist, Song } from './../entities/entities';
+
 import { PlaylistService } from './../services/playlist.service';
+import { SongService } from './../services/song.service';
 
 @Component({
   selector: 'app-search',
@@ -8,22 +10,23 @@ import { PlaylistService } from './../services/playlist.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  valor: any = { playlistName: '' };
   playlists: Playlist[];
-  playlistFilter: any = { playlistName : '' };
-  constructor(private playlistService: PlaylistService) { }
+  songs: Song[];
+  
+  constructor(private playlistService: PlaylistService, private songService: SongService) { }
 
   ngOnInit() {
-    this.playlistService.getPlaylists()
-      .subscribe(
-      playlists => this.playlists = playlists,
-      err => { console.log(err) });
+    this.playlistService.getPlaylists().subscribe(playlists => this.playlists = playlists, err => { console.log(err) });
+    this.songService.getSongs().subscribe(songs => this.songs = songs, err => { console.log(err) });
+
   }
-/*  searchValue = '';
-
-  onKey(event: any) { // without type info
-    this.searchValue = event.target.value;
-    console.log(this.searchValue);
-
-  }*/
+  /*  searchValue = '';
+  
+    onKey(event: any) { // without type info
+      this.searchValue = event.target.value;
+      console.log(this.searchValue);
+  
+    }*/
 
 }
