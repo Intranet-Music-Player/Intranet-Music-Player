971f09e83da9bf14a9acccdfc92b4ce143575393
OEEI <Othmane.El-Ati@gft.com>
2017-04-05 09:30:30 +0200

    Merge correct

--- Front-end-intranet/src/app/services/song.service.ts
+++ Front-end-intranet/src/app/services/song.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Song } from './../entities/entities';
+import { Song } from 'app/entities/entities';
 
 @Injectable()
 export class SongService {
